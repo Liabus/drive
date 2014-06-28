098800160f0ab81d1cc929c9b1bc56c4b484658e
Jordan Gensler <jordangens@gmail.com>
2014-06-28 14:09:26 -0500

    Starting with animation loop

--- drive.js
+++ drive.js
@@ -1,10 +1,40 @@
+//requestAnimationFrame Shim:
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
 (function(global){
 
   var count = 0;
 
-  function timeGen(tree, relTo, val){
+  function timeGen(tree, relTo, val, $el){
+    if(val === 'height'){
+      return $el.height();
+    };
     if(!relTo || !tree[relTo]) return val;
-    var vals = val.split('.');
+    var vals = relTo.split('.');
     var t = tree[vals[0]];
     var type = 'start';
     if(vals.length > 1){
@@ -20,20 +50,23 @@
     var active = [];
 
     // Find the active animations
-    for(var i  = 0; i < animations.length; i++) {
+    for(var i = 0; i < animations.length; i++){
       // If the position is within the range of animation
-      if(animations[i].start <=  pos && animations[i] >= pos) {
+      if(animations[i].start <= pos && animations[i].end >= pos) {
         active.push(animations[i]);
-      }
-
-      // If the animation start times are past the position,
-      // then the possible animations have run out
-      if(animations[i].start > pos) {
-        return active;
       }
     }
 
     return active;
+  };
+  
+  function getMaxHeight(animations){
+    var h = 0;
+    for(var i = 0; i < animations.length; i++){
+      
+      h = Math.max(h, animations[i].end);
+    }
+    return h;
   };
 
   function preventDefault(e) {
@@ -57,6 +90,8 @@
 
     //Setup scrolling intercept:
     var scrollPos = 0;
+    
+    var maxHeight = 0;
 
     function keydown(e) {
       for (var i = keys.length; i--;) {
@@ -69,7 +104,7 @@
           return;
         }
       }
-    }
+    };
 
     function scroll(e) {
       preventDefault(e);
@@ -77,21 +112,15 @@
       scrollPos += e.wheelDeltaY/10;
       
       if(scrollPos < 0) scrollPos = 0;
-      //TODO: Upper limit
-      
-      $(document.body).scrollTop(scrollPos);
-
-      console.log(scrollPos);
-    }
-
+      if(scrollPos > maxHeight) scrollPos = maxHeight;
+    };
+    
     if (window.addEventListener) {
         window.addEventListener('DOMMouseScroll', scroll, false);
     }
     window.onmousewheel = document.onmousewheel = scroll;
     document.onkeydown = keydown;
-
-
-
+    
 
     //Grab a reference to the element.
     var $el = $(selector);
@@ -118,25 +147,27 @@
     for(var i = 0; i < elements.length; i++){
       var el = elements[i];
 
-      //TODO:
-      var $el;
+      var $el = $(el.selector);
 
       //pull out parts:
-      var timeline = el.timeline;
       var animations = el.animations;
       
-      var relative = timeline.relative || el.relative || false;
+      var tl = el.timeline;
+      
+      var relative = tl.relative || el.relative || false;
       
       if(!el.name){
         el.name = 'drive-dy-' + ++count;
       }
 
-      tree[el.name] = {
-        //TODO: Check to make sure this height code works correctly:
+      var t = {
+        $: $el,
         height: $el.height(),
-        start: timeGen(tree, relative, timeline.start),
-        end: timeGen(tree, relative, timeline.start)
+        start: timeGen(tree, relative, tl.start, $el),
+        end: timeGen(tree, relative, tl.end, $el)
       };
+      
+      tree[el.name] = t;
 
       /*
       // TODO: Do we add the selector to the tree?
@@ -151,34 +182,50 @@
 
       // Loop through the animations
       for(var j = 0; j < animations.length; j++){
-        // Set the flag
-        inserted = false;
-
-        // Time sort the animations
-        for(var k = 0; k < timeline.length; k++) {
-          // Break ties with end times
-          if(animations[j].start === timeline[k].start && animations[j].end < timeline[k].end) {
-            timeline.splice(k, 0, animations[j]);
-            inserted = true;
-            break;
-          }
-
-          // If the animation is earlier than the one in the index, insert
-          if(animations[j].start < timeline[k].start) {
-            timeline.splice(k, 0, animations[j]);
-            inserted = true;
-            break;
-          }
-        }
-
-        // Otherwise insert at the end
-        if(inserted === false) {
-          timeline.push(animations[j]);
-        }
+        //Just push, we can sort later:
+        timeline.push({
+          $: $el,
+          element: el,
+          animation: animations[j],
+          start: t.start,
+          end: t.end
+        });
       };
     };
-
-    console.log(tree);
+    
+    maxHeight = getMaxHeight(timeline);
+    
+    
+    function animationLoop(){
+      requestAnimationFrame(animationLoop);
+      
+      var anims = getAnimationsForFrame(timeline, scrollPos);
+      var percent = 0;
+      var lc = '';
+      
+      for(var i = 0; i < anims.length; i++){
+        var an = anims[i];
+        
+        percent = (scrollPos) / (an.end - an.start);
+        lc = an.animation.property.toLowerCase();
+        
+        var unit = an.animation.start.slice(-1);
+        
+        var sv = parseInt(an.animation.start);
+        var ev = parseInt(an.animation.end);
+        var diff = sv - ev;
+        var neg = an.animation.start.charAt(0) === '-' ? true : false;
+        
+        //Generic Translatey/scroll property
+        if(lc === 'translatey' || lc === 'scroll'){
+          an.$.css('transform', 'translateY(' + (neg ? '-' : '') + (percent * diff) + unit + ')');
+        }
+      }
+      
+    };
+    
+    requestAnimationFrame(animationLoop);
+    
 
     return 'smile';
 
