d936b5b7bb5adf801dc7204a537beef80ff49d73
Jordan Gensler <jordangens@gmail.com>
2014-07-18 17:25:49 -0700

    File rename.

--- next/gulpfile.js
+++ next/gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 
 gulp.task('default', function () {
-    return gulp.src('src/app.js')
-        .pipe(traceur({sourceMap: true}))
-        .pipe(gulp.dest('dist'));
+  return gulp.src('src/app.js')
+    .pipe(traceur({sourceMap: true}))
+    .pipe(gulp.dest('dist'));
 });
